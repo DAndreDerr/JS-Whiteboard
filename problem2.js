9d69d996cf11e7efa3a44844e7b8cd93e3ba3293
DAndreDerr <dandre020727@gmail.com>
2023-04-18 19:33:41 -0400

    update

--- problem2.js
+++ problem2.js
@@ -1,7 +1,9 @@
 let exnum = 100;
 let exnum2 = 233;
 let exnum3 = 9;
-
+// the function below uses a modulo and a if else statement
+// if the modulo equals 0 this means the variable being tested is divisible by 10, this returns a true value
+// if the modulo doesn't equal 0 (else) it returns false
 function divisibletest(a) {
    if (a % 10 == 0) {
     return true
--- problem3.js
+++ problem3.js
@@ -1,5 +1,11 @@
 let exString = "This String has 7 vowels in it!"
-
+// this function utilizes regular expression (RegEx) with the match() method to find the number of vowels in the string
+// Once the match method gets the vowels the length property gives the number of vowels
+// finally we return the vowelcount
 function vowelcheck(a) {
-    const vowelcount = a.length
+
+    const vowelcount = a.match(/[aAeEiIoOuU]/gi).length;
+    return vowelcount;
 }
+
+console.log(vowelcheck(exString));
